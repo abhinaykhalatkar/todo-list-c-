61e553ee2b7e9165139bdbcafb72f0488aebde30
abhinaykhalatkar <abhinaykhalatkar@gmail.com>
2022-02-06 16:15:03 +0100

    2nd commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,28 +25,43 @@ namespace test
             InitializeComponent();
         }
 
-        private void taskNameTbx_KeyDown(object sender, KeyEventArgs e)
+        private void addToListBox(string item)
+
+        { 
+            taskLBx.Items.Add(item); 
+            taskTBx.Text = "";
+            addBtn.IsEnabled = false;
+        }
+
+        private void taskTBx_KeyDown(object sender, KeyEventArgs e)
         {
             addBtn.IsEnabled = true;
-            if(e.Key == Key.Enter)
+            if (e.Key == Key.Enter)
             {
-
+                addToListBox(taskTBx.Text);
+                
             }
-
         }
 
         private void addBtn_Click(object sender, RoutedEventArgs e)
-        {
+        { if(taskTBx.Text !="")
+            addToListBox(taskTBx.Text);
 
         }
 
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
+            if (!taskLBx.Items.IsEmpty)
+            {
+                taskLBx.Items.Remove(taskLBx.SelectedItem);
 
+                deleteBtn.IsEnabled = false;
+            }
+            
         }
-
         private void taskLBx_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            deleteBtn.IsEnabled = true;
 
         }
     }
